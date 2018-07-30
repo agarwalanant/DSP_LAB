1cc8b51e101206f8a5388e4a17726f2a84926d0d
Anant Agarwal <36265612+agarwalanant@users.noreply.github.com>
2018-07-30 11:33:32 +0530

    EXP1D

---
+++ LAB1/EXP1D.m
@@ -0,0 +1,16 @@
+n = -10:10;
+
+x = (0.8).^n;
+x_ = (0.8).^(-n);
+
+even = (x + x_)/2;
+
+subplot(2,1,1)
+stem(n,even)
+title('Even')
+
+odd = (x - x_)/2;
+
+subplot(2,1,2)
+stem(n,odd)
+title('odd')
