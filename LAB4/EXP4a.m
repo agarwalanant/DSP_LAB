4059ecde706acc037f8dbb6df3fbce5f5ac4cdb3
Anant Agarwal <36265612+agarwalanant@users.noreply.github.com>
2018-08-20 11:39:16 +0530

    EXP4 complete

--- LAB4/EXP4a.m
+++ LAB4/EXP4a.m
@@ -9,49 +9,53 @@ clear all
 prompt = 'Enter the sequence 1 ';
 x = input(prompt);
 
-
-x = [2 3 -1 4];
 N = length(x);
-X = zeros(4,1)
+X = zeros(N,1);
 for k = 0:N-1
     for n = 0:N-1
-        X(k+1) = X(k+1) + x(n+1)*exp(-j*pi/2*n*k)
+        X(k+1) = X(k+1) + x(n+1)*exp(-j*2*pi*k*n/N);
     end
 end
 
-t = 0:N-1
+t = 0:N-1;
 
-subplot(311)
+subplot(221)
 stem(t,x);
 xlabel('Time (s)');
 ylabel('Amplitude');
 title('Time domain - Input sequence')
 grid on
 
-subplot(312)
+subplot(222)
 stem(t,X)
 xlabel('Frequency');
 ylabel('|X(k)|');
 title('Frequency domain - Magnitude response')
 grid on
 
-subplot(313)
+subplot(223)
 stem(t,angle(X))
 xlabel('Frequency');
 ylabel('Phase');
 title('Frequency domain - Phase response')
-grid on
-X        
-angle(X)  
+grid on       
+dft = fft(x);  
 
 
-N1 = length(X);
+N1 = length(X)
 X1 = zeros(1,N1);
 for k = 0:N1-1
     for n = 0:N1-1
-        X1(k+1) = (X1(k+1) + (1/N1)*x(n+1)*exp(1j*2*pi*k*n/N1));
+        X1(k+1) = (X1(k+1) + (1/N1)*X(n+1)*exp(1j*2*pi*k*n/N1));
     end
     
 end
-ifft(X)
-X1
+ifft = ifft(X);
+
+subplot(224)
+stem(t,X1);
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('IDFT')
+
+grid on
