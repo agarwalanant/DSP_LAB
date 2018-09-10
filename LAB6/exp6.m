0fe3dae0f572a15c76c59054c0daca5237c27502
Anant Agarwal <36265612+agarwalanant@users.noreply.github.com>
2018-09-10 11:24:46 +0530

    part 1 complete

--- LAB6/exp6.m
+++ LAB6/exp6.m
@@ -6,27 +6,17 @@ close all
 
 x=input('Enter the sequence');
 n=input('Enter the number of sequence');
-
 x0 = x(1:2:n); 
-
 x1 = x(2:2:n); 
-
 X0 = fft(x0);
-
 X1 = fft(x1); 
-
 y=[X0,X1];
-
 W=[1 1 1 -1j];
-
 o=y.*W;
-
 oe=[o(2),o(4)];
 oo=[o(1),o(3)];
-
 OE=fft(oe);
 OO=fft(oo);
-
 z=[OE OO]
 z1= fft(x)
 stem(z);
