ab796245ea7afea497483108575072a636f1e7b7
Anant Agarwal <36265612+agarwalanant@users.noreply.github.com>
2018-10-06 11:33:17 +0530

    Final commit

--- Time and Freq domain of filters/EXP7B.m
+++ Time and Freq domain of filters/EXP7B.m
@@ -5,52 +5,50 @@ close all
 
 
 
-N=1024;
-x=0:512;
-xb =0:0.0625:32;
+N=512;
+x=0:32:1024;
 black = 0.42-0.5*cos(2*pi*x/(N-1))+0.8*cos(4*pi*x/(N-1));
 subplot(2,4,1)
-plot(x,black);
+stem(x,black);
 title('Blackman (Time Domain)');
 grid on;
 subplot(2,4,5)
-plot(xb,mag2db(fft(black)));
+stem(x,mag2db(fftshift(fft(black))/max(fft(black))));
 title('Blackman (Frequency Domain)');
 grid on;
 
-xn=0:1024;
-
+xn=0:32:1024;
 hann = 0.5*(1-cos(2*pi*xn/N));
 subplot(2,4,2)
-plot(xn,hann);
+stem(xn,hann);
 title('hanning (Time Domain)');
 grid on;
 subplot(2,4,6)
-plot(xn,mag2db(fft(hann)));
+stem(xn,mag2db(fftshift(fft(hann))/max(fft(hann))));
 title('Hanning (Frequency Domain)');
 grid on;
 
 hamm = 0.54 - 0.46*cos(2*pi*xn/N);
 subplot(2,4,3)
-plot(xn,hamm);
+stem(xn,hamm);
 title('Hamming (Time Domain)');
 grid on;
 subplot(2,4,7)
-plot(xn,mag2db(fft(hamm)));
+stem(xn,mag2db(fftshift(fft(hamm))/max(fft(hamm))));
 title('Hamming (Frequency Domain)');
 grid on;
 
-x=-512:512;
+x=-512:32:512;
 a = 1*(x>=-512);
 b= 1*(x<=512);
 rect = a.*b;
 
 subplot(2,4,4)
-plot(x,rect);
+stem(x,rect);
 title('Reactangle (Time Domain)');
 grid on;
 subplot(2,4,8)
-plot(x,mag2db(fft(rect)));
+stem(x,mag2db(fft(rect)/max(fft(rect))));
 title('Rectangle (Frequency Domain)');
 grid on;
 
