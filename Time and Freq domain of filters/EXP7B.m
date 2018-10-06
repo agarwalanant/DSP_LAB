c38fc2763342b42530349b5f10c3325f5f01ae76
Anant Agarwal <36265612+agarwalanant@users.noreply.github.com>
2018-10-06 11:40:40 +0530

    Complete Experiment

--- Time and Freq domain of filters/EXP7B.m
+++ Time and Freq domain of filters/EXP7B.m
@@ -3,18 +3,20 @@ clc
 clear all
 close all
 
-
-
 N=512;
 x=0:32:1024;
 black = 0.42-0.5*cos(2*pi*x/(N-1))+0.8*cos(4*pi*x/(N-1));
 subplot(2,4,1)
 stem(x,black);
 title('Blackman (Time Domain)');
+xlabel('Samples')
+ylabel('Amplitude')
 grid on;
 subplot(2,4,5)
 stem(x,mag2db(fftshift(fft(black))/max(fft(black))));
 title('Blackman (Frequency Domain)');
+xlabel('Normalized frequency (x pi rad/s)')
+ylabel('Amplitude')
 grid on;
 
 xn=0:32:1024;
@@ -22,20 +24,28 @@ hann = 0.5*(1-cos(2*pi*xn/N));
 subplot(2,4,2)
 stem(xn,hann);
 title('hanning (Time Domain)');
+xlabel('Samples')
+ylabel('Amplitude')
 grid on;
 subplot(2,4,6)
 stem(xn,mag2db(fftshift(fft(hann))/max(fft(hann))));
 title('Hanning (Frequency Domain)');
+xlabel('Normalized frequency (x pi rad/s)')
+ylabel('Amplitude')
 grid on;
 
 hamm = 0.54 - 0.46*cos(2*pi*xn/N);
 subplot(2,4,3)
 stem(xn,hamm);
 title('Hamming (Time Domain)');
+xlabel('Samples')
+ylabel('Amplitude')
 grid on;
 subplot(2,4,7)
 stem(xn,mag2db(fftshift(fft(hamm))/max(fft(hamm))));
 title('Hamming (Frequency Domain)');
+xlabel('Normalized frequency (x pi rad/s)')
+ylabel('Amplitude')
 grid on;
 
 x=-512:32:512;
@@ -46,10 +56,14 @@ rect = a.*b;
 subplot(2,4,4)
 stem(x,rect);
 title('Reactangle (Time Domain)');
+xlabel('Samples')
+ylabel('Amplitude')
 grid on;
 subplot(2,4,8)
 stem(x,mag2db(fft(rect)/max(fft(rect))));
 title('Rectangle (Frequency Domain)');
+xlabel('Normalized frequency (x pi rad/s)')
+ylabel('Amplitude')
 grid on;
 
 
